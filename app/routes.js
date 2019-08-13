ca1bf0ee383a2e5dd75d491b1b3c5c24beca1cab
Dean Smith <smith.dean@talk21.com>
2019-08-13 15:41:10 +0100

    Add address routing for v1

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,22 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 // Branching
+
+router.post('/v1/address-to-write-to', function (req, res) {
+    // Get the answer from session data
+    // The name between the quotes is the same as the 'name' attribute on the input elements
+    // However in JavaScript we can't use hyphens in variable names
+  
+    let differentAddress = req.session.data['different-address']
+  
+    if (differentAddress === 'yes') {
+      res.redirect('/v1/address-to-write-to')
+    } else {
+      res.redirect('/v1/confirmation')
+    }
+  })
+
+// Branching
 router.post('/v2/address-to-write-to', function (req, res) {
     // Get the answer from session data
     // The name between the quotes is the same as the 'name' attribute on the input elements
