95acb9bb7e98a4898c97350c52db3057cdc1f927
leah smith <leahsmith1104@gmail.com>
2019-09-20 16:02:39 +0100

    new master application

--- app/routes.js
+++ app/routes.js
@@ -135,6 +135,17 @@ router.post('/v12/letter-address', function (req, res) {
   }
 })
 
+router.post('/v12/enter-letter-address', function (req, res) {
+  
+  let extraAddress = req.session.data['address-confirmation']
+
+  if (extraAddress === 'address-not-listed') {
+    res.redirect('/v12/enter-letter-address')
+  } else {
+    res.redirect('/v12/declaration')
+  }
+})
+
 router.post('/v12/declaration', function (req, res) {
   
   let extraAddress = req.session.data['extra-address']
@@ -349,6 +360,210 @@ router.post('/as-is/children', function (req, res) {
     res.redirect('/as-is/partner-details')
   } else {
     res.redirect('/as-is/children')
+  }
+})
+
+// Branching current as is journey
+
+router.post('/master/eligibility-uc', function (req, res) {
+
+  let socialfundDebt = req.session.data['socialfund-debt']
+
+  if (socialfundDebt === 'yes') {
+    res.redirect('/master/exit-sf-debt')
+  } else {
+    res.redirect('/master/eligibility-uc')
+  }
+})
+
+router.post('/master/eligibility-benefits', function (req, res) {
+
+  let claimUC = req.session.data['claim-uc']
+
+  if (claimUC === 'yes') {
+    res.redirect('/master/exit-uc')
+  } else {
+    res.redirect('/master/eligibility-benefits')
+  }
+})
+
+router.post('/master/eligibility-esa-duration', function (req, res) {
+
+  let benefitRecieved = req.session.data['benefit-received']
+
+  if (benefitRecieved === 'esa') {
+    res.redirect('/master/eligibility-esa-duration')
+  }
+  if (benefitRecieved === 'jsa') {
+    res.redirect('/master/eligibility-jsa-duration')
+  } 
+  if (benefitRecieved === 'is') {
+    res.redirect('/master/eligibility-is-duration')
+  }
+  if (benefitRecieved === 'pc') {
+    res.redirect('/master/eligibility-pc-duration')
+  }else {
+    res.redirect('/master/exit-benefits')
+  }
+})
+
+router.post('/master/eligibility-esa-type', function (req, res) {
+
+  let esaDuration = req.session.data['esa-duration']
+
+  if (esaDuration === 'more-than-6months') {
+    res.redirect('/master/eligibility-esa-type')
+  } else {
+    res.redirect('/master/eligibility-esa-partner')
+  }
+})
+
+router.post('/master/eligibility-jsa-type', function (req, res) {
+
+  let jsaDuration = req.session.data['jsa-duration']
+
+  if (jsaDuration === 'more-than-12-months') {
+    res.redirect('/master/eligibility-100')
+  }
+  if (jsaDuration === '6-to-12-months') {
+    res.redirect('/master/eligibility-jsa-type')
+  } else {
+    res.redirect('/master/eligibility-jsa-partner')
+  }
+})
+
+router.post('/master/eligibility-is-partner', function (req, res) {
+
+  let isDuration = req.session.data['is-duration']
+
+  if (isDuration === 'more-than-6months') {
+    res.redirect('/master/eligibility-100')
+  } else {
+    res.redirect('/master/eligibility-is-partner')
+  }
+})
+
+router.post('/master/eligibility-pc-partner', function (req, res) {
+
+  let pcDuration = req.session.data['pc-duration']
+
+  if (pcDuration === 'yes') {
+    res.redirect('/master/eligibility-100')
+  } else {
+    res.redirect('/master/eligibility-pc-partner')
+  }
+})
+
+router.post('/master/eligibility-esa-previous', function (req, res) {
+
+  let esaPartner = req.session.data['esa-partner']
+
+  if (esaPartner === 'yes') {
+    res.redirect('/master/eligibility-100')
+  } else {
+    res.redirect('/master/eligibility-esa-previous')
+  }
+})
+
+router.post('/master/eligibility-jsa-previous', function (req, res) {
+
+  let esaPartner = req.session.data['jsa-partner']
+
+  if (esaPartner === 'yes') {
+    res.redirect('/master/eligibility-100')
+  } else {
+    res.redirect('/master/eligibility-jsa-previous')
+  }
+})
+
+router.post('/master/eligibility-is-previous', function (req, res) {
+
+  let isPartner = req.session.data['is-partner']
+
+  if (isPartner === 'yes') {
+    res.redirect('/master/eligibility-100')
+  } else {
+    res.redirect('/master/eligibility-is-previous')
+  }
+})
+
+router.post('/master/eligibility-pc-previous', function (req, res) {
+
+  let pcPartner = req.session.data['pc-partner']
+
+  if (pcPartner === 'yes') {
+    res.redirect('/master/eligibility-100')
+  } else {
+    res.redirect('/master/eligibility-pc-previous')
+  }
+})
+
+router.post('/master/eligibility-100', function (req, res) {
+
+  let esaPrevious = req.session.data['esa-previous']
+  let jsaPrevious = req.session.data['jsa-previous']
+  let isPrevious = req.session.data['is-previous']
+  let pcPrevious = req.session.data['pc-previous']
+  let esaType = req.session.data['esa-type']
+  let jsaType = req.session.data['jsa-type']
+
+  if (esaPrevious === 'esa-no') {
+    res.redirect('/master/exit-benefits-duration')
+  }
+  
+  if (jsaPrevious === 'jsa-no') {
+    res.redirect('/master/exit-benefits-duration')
+  } 
+
+  if (isPrevious === 'is-no') {
+    res.redirect('/master/exit-benefits-duration')
+  }
+  
+  if (pcPrevious === 'pc-no') {
+    res.redirect('/master/exit-benefits-duration')
+  }
+
+  if (esaType === 'esa-contribution') {
+    res.redirect('/master/exit-eligibility-esa-contrib')
+  } 
+
+  if (jsaType === 'jsa-contribution') {
+    res.redirect('/master/exit-eligibility-jsa-contrib')
+  } else {
+    res.redirect('/master/eligibility-100')
+  }
+})
+
+router.post('/master/eligibility-industrial-action', function (req, res) {
+
+  let oneHundred = req.session.data['100-or-more']
+
+  if (oneHundred === '100-yes') {
+    res.redirect('/master/eligibility-industrial-action')
+  } else {
+    res.redirect('/master/exit-100')
+  }
+})
+
+router.post('/master/partner', function (req, res) {
+
+  let industrialAction = req.session.data['industrial-action']
+
+  if (industrialAction === 'ia-yes') {
+    res.redirect('/master/exit-industrial-action')
+  } else {
+    res.redirect('/master/partner')
+  }
+})
+
+router.post('/master/children', function (req, res) {
+
+  let hasPartner = req.session.data['partner']
+
+  if (hasPartner === 'partner-yes') {
+    res.redirect('/master/partner-details')
+  } else {
+    res.redirect('/master/children')
   }
 })
 
