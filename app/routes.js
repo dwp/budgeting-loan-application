f0d883d62968fd9cebae491f438d2325464ffdc7
Dean Smith <smith.dean@talk21.com>
2019-12-17 16:25:13 +0000

    remove notify

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,3 @@
-var NotifyClient = require('notifications-node-client').NotifyClient,
-    notify = new NotifyClient(process.env.NOTIFYAPIKEY);
-
 const express = require('express')
 const router = express.Router()
 
@@ -709,26 +706,6 @@ router.post('/master-v2/contact-details', function (req, res) {
     res.redirect('/master-v2/contact-details')
   }
 })
-
-//NOTIFY ROUTING
-// The URL here needs to match the URL of the page that the user is on
-// when they type in their email address
-router.post('/notify-test/email-contact-method', function (req, res) {
-  notify.sendEmail(
-    // this long string is the template ID, copy it from the template
-    // page in GOV.UK Notify. It’s not a secret so it’s fine to put it
-    // in your code.
-    '1fe76c1a-f2bd-4ce4-a62e-6012f10c83a0',
-    // `emailAddress` here needs to match the name of the form field in
-    // your HTML page
-    req.body.emailAddress
-  );
-
-  // This is the URL the users will be redirected to once the email
-  // has been sent
-  res.redirect('/notify-test/email-confirmation');
-
-});
 
 //BLAS ROUTING
 
